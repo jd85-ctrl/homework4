54f4e90cfde109b920b28ae679b890cc75f6f485
jd85-ctrl <jd85@iu.edu>
2026-09-19 21:27:35 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const number1 = Number(prompt("Enter the first number"));
+const number2 = Number(prompt("Enter the second number"));
+const number3 = Number(prompt("Enter the third number"));
+
+
+const average = (number1 + number2 + number3) / 3;
+
+console.log(`The average os ${number1} and ${number2} and ${number3} is ${average}`);
+
+const name  = "Bob";
+const fruit = "Bananas";
+const FavoriteSport = prompt(`What is your favorite sport? Your name is ${name} and your favorite fruit is ${fruit}.`);
+console.log(`${name}'s favorite fruit is ${fruit} and enjoys ${FavoriteSport}.`);
+
+console.log(new Date());
